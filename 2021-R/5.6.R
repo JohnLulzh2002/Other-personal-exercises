1e7d7350cf44fe57e7e1463fbd479a3005ea7b72
JohnLu-HP-Laptop <lzh20020204@126.com>
2021-05-07 01:39:41 +0800

    Create 5.6.R

---
+++ 2021-R/5.6.R
@@ -0,0 +1,23 @@
+mc <- mtcars
+fix(mc)
+plot(mc)
+plot(mc$wt)
+plot(mc$wt, mc$mpg, type = "h", lwd = 5)
+md <- mc[order(mc$wt), ]
+plot(md$wt, md$mpg, type = "l")
+plot(md$wt, md$mpg,
+    cex = 2, col = rainbow(7),
+    type = "o",
+    xlab = "wt", ylab = "mpg",
+    cex.lab=2
+)
+plot(md$wt, md$mpg,
+    cex = 2, col = rainbow(7),
+    type = "o",
+    xlab = "wt", ylab = "mpg",
+    cex.lab=2,
+	ann=F
+)
+title(xlab = "Weight",
+    col.lab = "orange",
+    cex.lab = 1.5, font.lab = 3)
